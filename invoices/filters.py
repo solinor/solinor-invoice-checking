171a04737b0c05fa520f9d2a90b1b7b457aae313
Olli Jarva <olli.jarva@solinor.fi>
2017-04-04 18:30:00 +0300

    Add ordering options

--- invoices/filters.py
+++ invoices/filters.py
@@ -9,6 +9,21 @@ choices = (
 class InvoiceFilter(django_filters.FilterSet):
     is_approved = django_filters.ChoiceFilter(null_label="Unknown", choices=choices)
 
+    o = django_filters.OrderingFilter(
+        fields = (
+            ("incorrect_entries_count", "incorrect_entries_count"),
+            ("total_hours", "total_hours"),
+            ("total_money", "total_money"),
+            ("bill_rate_avg", "bill_rate_avg"),
+        ),
+        field_labels= {
+            "incorrect_entries_count": "Issues",
+            "total_hours": "Hours",
+            "total_money": "Price",
+            "bill_rate_avg": "Bill rate (avg)",
+        }
+    )
+
     class Meta:
         model = Invoice
         fields = {
