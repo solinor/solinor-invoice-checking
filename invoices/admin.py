b99caaba6843770df47ee1bd369942fa64ebb488
Olli Jarva <olli.jarva@solinor.fi>
2017-05-08 17:15:06 +0300

    Improve admin UI fields and filters

--- invoices/admin.py
+++ invoices/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from invoices.models import AuthToken, ProjectFixedEntry, InvoiceFixedEntry, SlackChannel
+from invoices.models import AuthToken, ProjectFixedEntry, InvoiceFixedEntry, SlackChannel, Project
+
+
+class DeleteNotAllowedModelAdmin(admin.ModelAdmin):
+    def has_delete_permission(self, request, obj=None):
+        return False
 
 class AuthTokenAdmin(admin.ModelAdmin):
     pass
@@ -16,7 +21,15 @@ class InvoiceFixedEntryAdmin(admin.ModelAdmin):
 admin.site.register(InvoiceFixedEntry, InvoiceFixedEntryAdmin)
 
 
-class SlackChannelAdmin(admin.ModelAdmin):
-    pass
+class SlackChannelAdmin(DeleteNotAllowedModelAdmin):
+    list_display = ("channel_id", "name")
+    search_fields = ("name",)
 
 admin.site.register(SlackChannel, SlackChannelAdmin)
+
+class ProjectAdmin(DeleteNotAllowedModelAdmin):
+    fields = ("slack_channel", )
+    list_display = ("client", "name", "slack_channel")
+    search_fields = ("client", "name")
+
+admin.site.register(Project, ProjectAdmin)
