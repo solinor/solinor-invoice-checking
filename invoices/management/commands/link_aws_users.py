0faebf0ca77eef788206091d4ef16e5d5a8c5be6
Olli Jarva <olli.jarva@solinor.fi>
2017-05-13 00:29:35 +0300

    Automatically link 10kf users to AWS accounts

---
+++ invoices/management/commands/link_aws_users.py
@@ -0,0 +1,22 @@
+import datetime
+import json
+import tempfile
+import pytz
+
+import boto3
+
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+from django.conf import settings
+
+from invoices.models import AmazonLinkedAccount, FeetUser
+
+
+class Command(BaseCommand):
+    help = 'Link AWS accounts and 10kf users'
+
+    def handle(self, *args, **options):
+        for user in FeetUser.objects.all():
+            accounts = AmazonLinkedAccount.objects.filter(name=u'%s %s' % (user.first_name, user.last_name))
+            if len(accounts) == 1:
+                user.amazon_account.add(accounts[0])
