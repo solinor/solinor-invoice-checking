eacc15098122a1fcd2f020627be1b6bc72a44521
Olli Jarva <olli.jarva@solinor.fi>
2017-05-13 11:56:14 +0300

    Split calculating invoice stats to smaller parts

--- invoices/invoice_utils.py
+++ invoices/invoice_utils.py
@@ -15,7 +15,7 @@ def generate_amazon_invoice_data(linked_account, entries, year, month):
         "total_entries": total_entries,
     }
 
-def calculate_entry_stats(entries, fixed_invoice_rows, aws_entries=None):
+def calculate_stats_for_hours(entries):
     phases = {}
     billable_incorrect_price = []
     non_billable_hours = []
@@ -60,28 +60,7 @@ def calculate_entry_stats(entries, fixed_invoice_rows, aws_entries=None):
 
     if total_rows["hours"]["incurred_hours"] > 0:
         total_rows["hours"]["bill_rate_avg"] = total_rows["hours"]["incurred_money"] / total_rows["hours"]["incurred_hours"]
-    if len(fixed_invoice_rows) > 0:
-        total_rows["fixed"] = {"description": "Fixed rows", "incurred_money": 0}
-        phases["Fixed"] = {"entries": {}, "billable": True}
-        for item in fixed_invoice_rows:
-            phases["Fixed"]["entries"][item.description] = {"price": item.price, "currency": "EUR"}
-            total_rows["fixed"]["incurred_money"] += item.price
 
-    if aws_entries and len(aws_entries):
-        for aws_account, aws_entries in aws_entries.items():
-            account_key = "AWS: %s" % aws_account
-            phases[account_key] = {"entries": {}, "billable": True}
-            for aws_entry in aws_entries:
-                if aws_entry.record_type == "AccountTotal":
-                    total_key = "aws_%s" % aws_entry.currency
-                    if total_key not in total_rows:
-                        total_rows[total_key] = {"description": "Amazon billing (%s)" % aws_entry.currency, "incurred_money": 0, "currency": aws_entry.currency}
-
-                    total_rows[total_key]["incurred_money"] += aws_entry.total_cost
-                    phases[account_key]["entries"]["Amazon Web Services billing"] = {"price": aws_entry.total_cost, "currency": aws_entry.currency}
-                    break
-            else:
-                phases[account_key]["entries"]["Amazon Web Services billing"] = {"price": 0, "currency": "USD"}
 
     stats = {
         "total_rows": total_rows,
@@ -102,3 +81,56 @@ def calculate_entry_stats(entries, fixed_invoice_rows, aws_entries=None):
     }
     stats["incorrect_entries_count"] = stats["billable_incorrect_price_count"] + stats["non_billable_hours_count"] + stats["non_phase_specific_count"] + stats["not_approved_hours_count"] + stats["empty_descriptions_count"] + stats["no_category_count"]
     return stats
+
+def calculate_stats_for_fixed_rows(fixed_invoice_rows):
+    total_rows = {}
+    phases = {}
+    if len(fixed_invoice_rows) > 0:
+        total_rows["fixed"] = {"description": "Fixed rows", "incurred_money": 0}
+        phases["Fixed"] = {"entries": {}, "billable": True}
+        for item in fixed_invoice_rows:
+            phases["Fixed"]["entries"][item.description] = {"price": item.price, "currency": "EUR"}
+            total_rows["fixed"]["incurred_money"] += item.price
+    return {
+        "phases": phases,
+        "total_rows": total_rows,
+    }
+
+def calculate_stats_for_aws_entries(aws_entries):
+    phases = {}
+    total_rows = {}
+    if aws_entries and len(aws_entries):
+        for aws_account, aws_entries in aws_entries.items():
+            account_key = "AWS: %s" % aws_account
+            phases[account_key] = {"entries": {}, "billable": True}
+            for aws_entry in aws_entries:
+                if aws_entry.record_type == "AccountTotal":
+                    total_key = "aws_%s" % aws_entry.currency
+                    if total_key not in total_rows:
+                        total_rows[total_key] = {"description": "Amazon billing (%s)" % aws_entry.currency, "incurred_money": 0, "currency": aws_entry.currency}
+
+                    total_rows[total_key]["incurred_money"] += aws_entry.total_cost
+                    phases[account_key]["entries"]["Amazon Web Services billing"] = {"price": aws_entry.total_cost, "currency": aws_entry.currency}
+                    break
+            else:
+                phases[account_key]["entries"]["Amazon Web Services billing"] = {"price": 0, "currency": "USD"}
+    return {
+        "phases": phases,
+        "total_rows": total_rows,
+    }
+
+def combine_invoice_parts(*combine_stats):
+    stats = {}
+    for stat in combine_stats:
+        for k, v in stat.items():
+            if k not in stats:
+                stats[k] = v
+            else:
+                stats[k].update(v)
+    return stats
+
+def calculate_entry_stats(hour_entries, fixed_invoice_rows, aws_entries=None):
+    hour_stats = calculate_stats_for_hours(hour_entries)
+    fixed_invoice_stats = calculate_stats_for_fixed_rows(fixed_invoice_rows)
+    aws_stats = calculate_stats_for_aws_entries(aws_entries)
+    return combine_invoice_parts(hour_stats, fixed_invoice_stats, aws_stats)
