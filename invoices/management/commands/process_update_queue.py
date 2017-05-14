a2af18ef04ea9bb34f9562639ec6610a47909168
Olli Jarva <olli.jarva@solinor.fi>
2017-05-14 21:08:51 +0300

    Use logging instead of print statements

--- invoices/management/commands/process_update_queue.py
+++ invoices/management/commands/process_update_queue.py
@@ -1,6 +1,7 @@
 import datetime
 import redis
 import json
+import logging
 
 from django.core.management.base import BaseCommand
 from django.utils import timezone
@@ -14,16 +15,18 @@ class Command(BaseCommand):
     help = 'Import data from 10k feet API'
 
     def handle(self, *args, **options):
+        logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
+
         redis_instance = redis.from_url(settings.REDIS)
         pubsub = redis_instance.pubsub(ignore_subscribe_messages=True)
         pubsub.subscribe("request-refresh")
         for entry in pubsub.listen():
-            print entry
+            logger.debug("Received %s from redis queue", entry)
             now = timezone.now()
             try:
                 latest_run = DataUpdate.objects.exclude(finished_at=None).latest("finished_at")
                 if now - latest_run.finished_at < datetime.timedelta(seconds=10):
-                    print "Latest run was finished recently. Skip."
+                    logger.info("Latest run was finished recently. Skip this data update.")
                     latest_run.aborted = True
                     latest_run.save()
                     continue
@@ -43,14 +46,14 @@ class Command(BaseCommand):
             data = json.loads(entry["data"])
             start_date = datetime.datetime.strptime(data["start_date"], "%Y-%m-%d").date()
             end_date = datetime.datetime.strptime(data["end_date"], "%Y-%m-%d").date()
-            print "Updating data."
+            logger.info("Updating data.")
             update_obj.started_at = timezone.now()
             hour_entry_update = HourEntryUpdate(start_date, end_date)
             hour_entry_update.update()
-            print "Data update done."
-            print "Update statistics."
+            logger.info("Data update done.")
+            logger.info("Update statistics.")
             refresh_stats(start_date, end_date)
             update_obj.finished_at = timezone.now()
             update_obj.aborted = False
             update_obj.save()
-            print "Statistics updated."
+            logger.info("Statistics updated.")
