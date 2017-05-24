2633ab1fe1ddc6795565e221f14be39e6bb919ff
Olli Jarva <olli.jarva@solinor.fi>
2017-05-24 22:48:15 +0300

    Hack together per-person level to treemap

--- invoices/views.py
+++ invoices/views.py
@@ -379,6 +379,9 @@ def hours_charts(request):
     entries_for_past_month = HourEntry.objects.filter(date__gte=month_ago, date__lte=today).order_by("project").values("project").annotate(hours=Sum("incurred_hours")).values("project", "client", "hours")
     entries_before_past_month =  HourEntry.objects.filter(date__lte=month_ago, date__gte=two_months_ago).order_by("project").values("project").annotate(hours=Sum("incurred_hours")).values("project", "client", "hours")
 
+    per_person_entries_for_past_month = HourEntry.objects.exclude(client="[none]").filter(date__gte=month_ago, date__lte=today).order_by("project").values("project", "user_name").annotate(hours=Sum("incurred_hours")).values("user_name", "project", "client", "hours")
+    per_person_entries_before_past_month = HourEntry.objects.exclude(client="[none]").filter(date__lte=month_ago, date__gte=two_months_ago).order_by("project").values("project", "user_name").annotate(hours=Sum("incurred_hours")).values("user_name", "project", "client", "hours")
+
     per_project_data = {}
     for entry in entries_for_past_month:
         k = "%s - %s" % (entry["client"], entry["project"])
@@ -389,14 +392,29 @@ def hours_charts(request):
         if k in per_project_data:
             per_project_data[k]["2m"] = entry
 
-
     for entry in per_project_data.values():
         if "2m" in entry:
             diff = entry["2m"]["hours"] - entry["1m"]["hours"]
         else:
             diff = 0
-
         data.append((entry["1m"]["project"], entry["1m"]["client"], entry["1m"]["hours"], diff))
+
+    per_user_data = {}
+    for entry in per_person_entries_for_past_month:
+        k = "%s - %s - %s" % (entry["user_name"], entry["client"], entry["project"])
+        per_user_data[k] = {"1m": entry}
+
+    for entry in per_person_entries_before_past_month:
+        k = "%s - %s - %s" % (entry["user_name"], entry["client"], entry["project"])
+        if k in per_user_data:
+            per_user_data[k]["2m"] = entry
+
+    for entry in per_user_data.values():
+        if "2m" in entry:
+            diff = entry["2m"]["hours"] - entry["1m"]["hours"]
+        else:
+            diff = 0
+        data.append((u"%s - %s" % (entry["1m"]["user_name"], entry["1m"]["project"]), entry["1m"]["project"], entry["1m"]["hours"], diff))
 
     treemaps.append(("projects_treemap", "Projects for past 30 days", json.dumps(data)))
 
