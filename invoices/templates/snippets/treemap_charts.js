b3164580c70c0ae7046b02e7a2fef99eff209bf2
Olli Jarva <olli.jarva@solinor.fi>
2017-05-25 21:10:55 +0300

    Add value tooltips to treemaps

--- invoices/templates/snippets/treemap_charts.js
+++ invoices/templates/snippets/treemap_charts.js
@@ -8,8 +8,17 @@ var options = {
  headerHeight: 15,
  fontColor: 'black',
  showScale: true,
- height: 600
+ height: 600,
+ generateTooltip: showFullTooltip
 };
 var chart = new google.visualization.TreeMap(document.getElementById('{{ treemap.0 }}'));
 chart.draw(data, options);
+
 {% endfor %}
+
+function showFullTooltip(row, size, value) {
+  return '<div style="background:#fd9; padding:10px; border-style:solid">' +
+         '<span style="font-family:Courier"><b>' + data.getValue(row, 0) +
+         '</b>, ' + data.getValue(row, 1) + ', value: ' + size +
+         ', diff from past month: ' + data.getValue(row, 3) + ' </div>';
+}
