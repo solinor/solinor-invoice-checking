c2ab73da3ebecd496fb9b58c79fab14804297665
Olli Jarva <olli.jarva@solinor.fi>
2018-01-10 21:46:57 +0200

    Update flex hours calculations
    - Properly exclude unpaid holidays
    - Don't count any leaves during weekends and public holidays to flex saldo
    - Show leave hours in calculation log

--- flex_hours/utils.py
+++ flex_hours/utils.py
@@ -75,14 +75,17 @@ def calculate_flex_saldo(person):
     holidays_list = PublicHoliday.objects.filter(date__gte=start_hour_markings_from_date).filter(date__lte=today)
     holidays = {k.date: k.name for k in holidays_list}
 
-    hour_markings_list = list(HourEntry.objects.filter(user_m=person).filter(date__gte=start_hour_markings_from_date).exclude(date__gte=today).exclude(leave_type="Flex time Leave").order_by("date").values("date").annotate(incurred_hours=Sum("incurred_hours")).values_list("date", "incurred_hours"))
+    hour_markings_list = list(HourEntry.objects.filter(user_m=person).filter(date__gte=start_hour_markings_from_date).exclude(date__gte=today).filter(leave_type="[project]").order_by("date").values("date").annotate(incurred_hours=Sum("incurred_hours")).values_list("date", "incurred_hours"))
+    leave_markings_list = list(HourEntry.objects.filter(user_m=person).filter(date__gte=start_hour_markings_from_date).exclude(date__gte=today).exclude(leave_type="Flex time Leave").exclude(leave_type="[project]").exclude(leave_type="Unpaid leave").order_by("date").values("date").annotate(incurred_hours=Sum("incurred_hours")).values_list("date", "incurred_hours"))
     hour_markings = {k[0]: float(k[1]) for k in hour_markings_list}
+    leave_markings = {k[0]: float(k[1]) for k in leave_markings_list}
 
     last_process_day = find_last_process_date(hour_markings_list, contracts)
     current_day = start_hour_markings_from_date
     calculation_log = []
     daily_diff_entries = []
     while current_day <= last_process_day:
+        is_weekend = is_holiday = False
         message_for_today = ""
         for event in events:
             if event.date == current_day and event.adjust_by:
@@ -97,6 +100,7 @@ def calculate_flex_saldo(person):
 
         if 0 < current_day.isoweekday() < 6:
             if current_day in holidays:
+                is_holiday = True
                 message_for_today += "Public holiday: %s. " % holidays[current_day]
             else:
                 flex_hour_deduct = (float(contract.worktime_percent or 100) / 100) * 7.5
@@ -104,6 +108,7 @@ def calculate_flex_saldo(person):
                     flex_hours -= flex_hour_deduct
                     message_for_today += "Deducting normal workday: -7.5 * %s%% = -%sh. " % (contract.worktime_percent or 100, flex_hour_deduct)
         elif plus_hours_today:
+            is_weekend = True
             message_for_today += "Weekend. "
 
         if contract.flex_enabled and plus_hours_today:
@@ -111,6 +116,16 @@ def calculate_flex_saldo(person):
             message_for_today += "Adding hour markings: %sh. " % plus_hours_today
         elif not contract.flex_enabled and (plus_hours_today or flex_hour_deduct):
             message_for_today += "Flex time is not enabled. Would have been +%sh and -%sh for today." % (plus_hours_today, flex_hour_deduct)
+        if current_day in leave_markings:
+            leave_hours = leave_markings[current_day]
+            if is_weekend:
+                message = " Also, %s hours as leave markings, not counted for weekend."
+            elif is_holiday:
+                message = " Also, %s hours as leave markings, not counted for public holidays."
+            else:
+                message = " Also, %s hours as leave markings."
+                flex_hours += leave_hours
+            message_for_today += message % leave_hours
 
         if message_for_today:
             calculation_log.append({"date": current_day, "message": message_for_today})
