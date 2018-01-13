6133a9a8598cbab4973eac742404f1d890de17b0
Olli Jarva <olli.jarva@solinor.fi>
2018-01-13 19:20:16 +0200

    Add flex saldo to top bar
    With current logic, saldo calculation takes ~500ms, which is unprohibitively slow for normal pageload -> load asynchronously

--- flex_hours/views.py
+++ flex_hours/views.py
@@ -1,18 +1,36 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import json
+
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 
 from flex_hours.utils import FlexHourException, calculate_flex_saldo
 from invoices.models import TenkfUser
 
 
-@login_required
-def person_flex_hours(request, user_guid):
-    person = get_object_or_404(TenkfUser, guid=user_guid)
+def get_flex_hours_for_user(request, person):
+    json_responses = "application/json" in request.META.get('HTTP_ACCEPT')
     try:
         context = calculate_flex_saldo(person)
     except FlexHourException as error:
+        if json_responses:
+            return HttpResponse(json.dumps({"flex_enabled": False}), content_type="application/json")
         return render(request, "error.html", {"error": error, "message": "This is normal for flex hour calculations when some required information is missing. If this is your page, please contact HR to get this fixed."})
+    if json_responses:
+        return HttpResponse(json.dumps({"flex_enabled": True, "flex_hours": context["flex_hours"]}), content_type="application/json")
     return render(request, "person_flex_hours.html", context)
+
+
+@login_required
+def person_flex_hours(request, user_guid):
+    person = get_object_or_404(TenkfUser, guid=user_guid)
+    return get_flex_hours_for_user(request, person)
+
+
+@login_required
+def your_flex_hours(request):
+    person = get_object_or_404(TenkfUser, email=request.user.email)
+    return get_flex_hours_for_user(request, person)
