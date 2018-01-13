49ae35956725c318decc927925e5ed87e015ddf8
Olli Jarva <olli.jarva@solinor.fi>
2018-01-14 00:38:05 +0200

    Allow adjusting calendar chart height per view

--- invoices/templates/snippets/calendar_chart.js
+++ invoices/templates/snippets/calendar_chart.js
@@ -2,13 +2,15 @@ var dataTable = new google.visualization.DataTable();
 dataTable.addColumn({ type: 'date', id: 'Date' });
 dataTable.addColumn({ type: 'number', id: '{{ number_header }}' });
 dataTable.addColumn({ type: 'string', role: 'tooltip', 'p': {'html': true} });
+
 dataTable.addRows([
   {% for entry in entries %}[new Date({{ entry.0 }}, {{ entry.1 }}, {{ entry.2 }}), {{ entry.3|floatformat:2 }}, "<div style='padding:5px 5px 5px 5px;'><h4>{{ entry.4 }}</h4></div>"], {% endfor %}
 ]);
 var chart = new google.visualization.Calendar(document.getElementById('{{ destination_id }}'));
 var options = {
   title: "{{ title }}",
-  height: 350,
+  height: {% if calendar_height %}{{ calendar_height }}{% else %}350{% endif %},
   tooltip: {isHtml: true}
 };
+
 chart.draw(dataTable, options);
