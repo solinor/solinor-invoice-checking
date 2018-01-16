c543a6942b91d5bdfd0a47055b9c31fe123ad8cb
Olli Jarva <olli.jarva@solinor.fi>
2018-01-16 22:18:05 +0200

    Add weekday checks to notification queue commands

--- invoices/management/commands/queue_unsubmitted_hours_notifications.py
+++ invoices/management/commands/queue_unsubmitted_hours_notifications.py
@@ -8,6 +8,18 @@ from django.core.management.base import BaseCommand
 class Command(BaseCommand):
     help = 'Queue notifications for hours that were not submitted'
 
+    def add_arguments(self, parser):
+        # Named (optional) arguments
+        parser.add_argument(
+            "--force",
+            action="store_true",
+            dest="force",
+            help="Force queuing instead of checking weekday",
+        )
+
     def handle(self, *args, **options):
-        redis_client = redis.from_url(settings.REDIS)
-        redis_client.publish("request-refresh", json.dumps({"type": "slack-unsubmitted-notification"}))
+        if options.get("force") or timezone.now().isoweekday() == 1:
+            redis_client = redis.from_url(settings.REDIS)
+            redis_client.publish("request-refresh", json.dumps({"type": "slack-unsubmitted-notification"}))
+        else:
+            self.stdout.write("No force option specified, and it is not Monday - notifications not queued.")
