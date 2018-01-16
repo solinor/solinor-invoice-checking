a24a6fdd56e00380a2f159e074d7074f0c6ef34e
Olli Jarva <olli.jarva@solinor.fi>
2018-01-16 19:46:26 +0200

    Remove deprecated URL

--- invoices/static/js/header_flex_hours.js
+++ invoices/static/js/header_flex_hours.js
@@ -1,5 +1,5 @@
 $(function () {
-  $.get("/your/flexhours", null, function (data) {
+  $.get("/you/flexhours", null, function (data) {
     if (data.flex_enabled === true && "flex_hours" in data) {
       $("#flex_hours_count").html(data.flex_hours + "h").removeClass("label-default").addClass("label-success");
     }
