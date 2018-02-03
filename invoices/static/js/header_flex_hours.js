1923f1490712cfbfe61b81c9850879fad894b62d
Olli Jarva <olli.jarva@solinor.fi>
2018-02-04 00:00:19 +0200

    Fix page onload javascripts to work with turbolinks

--- invoices/static/js/header_flex_hours.js
+++ invoices/static/js/header_flex_hours.js
@@ -1,4 +1,4 @@
-$(function () {
+$(document).on('turbolinks:load', function () {
   $.get("/you/flexhours/json", null, function (data) {
     if (data.flex_enabled === true && "flex_hours" in data) {
       $("#flex_hours_count").html(data.flex_hours.toFixed(0) + "h").removeClass("label-default").addClass("label-success");
