dca2f026239aff47a045bae4061a98c1f96c03eb
Olli Jarva <olli.jarva@solinor.fi>
2018-02-05 10:48:03 +0200

    Fix processing requests with anonymous users

--- invoices/context_processors.py
+++ invoices/context_processors.py
@@ -2,7 +2,7 @@ from invoices.models import TenkfUser
 
 
 def add_tenkfuser(request):
-    if request.user:
+    if request.user and request.user.id is not None:
         try:
             return {
                 "tenkf_user": TenkfUser.objects.get(email=request.user.email)
