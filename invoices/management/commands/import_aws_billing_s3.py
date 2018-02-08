aeda252fc742e79e99a10978659b93e2cdbb9658
Olli Jarva <olli.jarva@solinor.fi>
2018-02-08 16:33:22 +0200

    Automatically add leading zeros to months

--- invoices/management/commands/import_aws_billing_s3.py
+++ invoices/management/commands/import_aws_billing_s3.py
@@ -11,12 +11,12 @@ class Command(BaseCommand):
     help = "Import AWS billing CSV from S3"
 
     def add_arguments(self, parser):
-        parser.add_argument("year", nargs=1, type=str)
-        parser.add_argument("month", nargs=1, type=str)
+        parser.add_argument("year", nargs=1, type=int)
+        parser.add_argument("month", nargs=1, type=int)
 
     def handle(self, *args, **options):
         year = options["year"][0]
-        month = options["month"][0]
+        month = "{:02d}".format(options["month"][0])
         s3_client = boto3.client("s3", aws_access_key_id=settings.AWS_ACCESS_KEY, aws_secret_access_key=settings.AWS_SECRET_KEY)
         with tempfile.NamedTemporaryFile(mode="w+b") as data:
             s3_client.download_fileobj("solinor-hostmaster-billing", f"321914701408-aws-billing-csv-{year}-{month}.csv", data)
