76d53d2b6715ccd2c88f8fc070dcc1b78f4afe57
Olli Jarva <olli.jarva@solinor.fi>
2018-02-11 21:01:34 +0200

    Update flex saldo badge colors

--- invoices/static/js/header_flex_hours.js
+++ invoices/static/js/header_flex_hours.js
@@ -1,7 +1,13 @@
 $(document).on('turbolinks:load', function () {
   $.get("/you/flexhours/json", null, function (data) {
     if (data.flex_enabled === true && "flex_hours" in data) {
-      $("#flex_hours_count").html(data.flex_hours.toFixed(0) + "h").removeClass("label-default").addClass("label-success");
+      var flex_hours_count = $("#flex_hours_count");
+      flex_hours_count.html(data.flex_hours.toFixed(0) + "h");
+      if (data.flex_hours > 120) {
+        flex_hours_count.removeClass("badge-secondary").addClass("badge-warning");
+      } else if (data.flex_hours < -40) {
+        flex_hours_count.removeClass("badge-secondary").addClass("badge-danger");
+      }
     }
   }, "json");
 });
